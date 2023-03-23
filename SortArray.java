3d4ebe83a86ee021111161fd90c2f78158c488e7
22C006 <22c006@gectcr.ac.in>
2023-03-23 15:48:21 +0530

    Array Sum file was added. Sort Array program has been started.

---
+++ SortArray.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+
+public class SortArray {
+
+    public static void Sort() {
+
+        int swap;
+        int size;
+        
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter Number of Elements:");
+        size = scan.nextInt();
+
+        int inputArray [] = new int[size];
+
+        System.out.println("Enter " +  size + " Elements");
+        for(int i = 0; i < size; i++) {
+            inputArray[i] = scan.nextInt();
+        }
+
+        for(int i = 0; i < size; i++){
+            
+        }
+        
+    }
+    
+}
